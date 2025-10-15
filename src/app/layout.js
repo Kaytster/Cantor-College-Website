450448c9707ac24fb8e40f9d398ff4b2f1088e8f
Katy <ksharp2405@gmail.com>
2025-10-15 09:43:10 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
-import Header from "@/app/components/header";
+import Header from "./components/header";
 import Footer from "./components/footer";
 import Link from "next/link";
-import "@/app/global.css";
-import "@/app/styles/mobile.css";
-import "@/app/styles/desktop.css";
+import "./global.css";
+import "./styles/mobile.css";
+import "./styles/desktop.css";
 
 export default function RootLayout({ children }) {
   return (
